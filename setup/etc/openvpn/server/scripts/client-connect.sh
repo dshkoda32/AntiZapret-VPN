4697d4a6e55d8a107bc376e4c58e3fb88014fa23
GubernievS <26377864+GubernievS@users.noreply.github.com>
2026-04-13 11:43:21 +0300

    Update client-connect.sh

--- setup/etc/openvpn/server/scripts/client-connect.sh
+++ setup/etc/openvpn/server/scripts/client-connect.sh
@@ -12,6 +12,8 @@ fi
 
 if [[ "$IV_PLAT" != 'linux' ]]; then
 	echo 'push "ifconfig-ipv6 fdea:dead::2/64 fdea:dead::1"
+push "route-ipv6 ::/1"
+push "route-ipv6 8000::/1"
 push "route-ipv6 2000::/3"
 push "block-ipv6"' >> "$1"
 fi
